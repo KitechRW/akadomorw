dda8d676c579a72427dbaeedb0d448798d0bf697
fabriceumuhire <fabrice.umuhire.ubugeni@gmail.com>
2021-02-12 09:38:11 +0200

    Website for bukreative

--- bukreative/script.js
+++ bukreative/script.js
@@ -40,40 +40,36 @@ function includeHTML() {
 
 	// In menu section, the first section below navbar
 	document.querySelector('#company-title').innerHTML = 'Bukreative';
-	document.querySelector('#company-desc').innerHTML = `
-					We Sell Children clothes, Shoes of all ages from toddler to 14 years old, both boys and girls.
-					We also sell toys, Pampers, dolls,  carpets and many more.`;
+	document.querySelector('#company-desc').innerHTML = `We are specialized in fashion, we ensure that you look good and feel good for every occasion!`;
 
 	// In who we are section
 	document.querySelector(
 		'#status'
 	).innerHTML = `
 	We all have something things we are passionate about. But Pursuing those things isn't all rainbows and colours because let’s be real! <br />We already are busy with life itself,crowding our minds with our needs at the moment and finding survival, making the idea of working on personal projects is more of a luxury we can not afford. Reasons like" it’s just not the right time"or "I don't have enough capital or investment"…if you are looking for reasons, you will lose count.
-	<br />I personally went through all of it until my will  to suppress the optimistic side of me worn out and I couldn’t help not doing what I like, what I do effortlessly, what I love to see others do, which is making sure people look good.
-	I am aware of the differences in people's personalities,the diversity of their mentalities about fashion and I am ready to yet explore these choices keeping my own touch upon each of them.
-	`;
+	<br />I personally went through all of it until my will  to suppress the optimistic side of me worn out and I couldn’t help not doing what I like, what I do effortlessly, what I love to see others do, which is making sure people look good.`;
 
 	// Abous us section
 	// left section in about us
 	document.querySelector(
 		'#mission'
-	).innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magni asperiores velit!`;
+	).innerHTML = `At Bukreative, we are aware of the differences in people's personalities,the diversity of their mentalities about fashion and we are ready to yet explore these choices keeping our own touch upon each of them.`;
 
 	// middle section in about us
 	document.aboutimg.src = './images/logo1.jpg';
 	document.querySelector(
 		'#img-caption'
-	).innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magni asperiores velit!`;
+	).innerHTML = `Bukreative exists for the love of fashion. We believe in empowering individuality`;
 
 	// right section in about us
 	document.querySelector(
 		'#visions'
-	).innerHTML = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero magni asperiores velit!`;
+	).innerHTML = `Ensure people look good fashion wise`;
 
 	// In team section
 	document.firstimg.src = './images/avatar.jpg';
-	document.querySelector('#first-name').innerHTML = 'Uwiringiye Sophie';
-	document.querySelector('#first-position').innerHTML = 'Co founder & CEO';
+	document.querySelector('#first-name').innerHTML = 'Uwase Christiane';
+	document.querySelector('#first-position').innerHTML = 'Founder & CEO';
 
 	/* document.secondimg.src = './images/avatar.jpg';
 	document.querySelector('#second-name').innerHTML = 'William Haven';
@@ -88,16 +84,16 @@ function includeHTML() {
 	document.querySelector('#fouth-position').innerHTML = 'Sales Manager'; */
 
 	// In contact us section
-	document.querySelector('#address').innerHTML = 'Kigali City Tower';
-	document.querySelector('#street').innerHTML = 'KN 2 Avenue';
-	document.querySelector('#email').innerHTML = 'sobirikunzira@gmail.com';
-	document.querySelector('#phone').innerHTML = '+250788374674';
+	document.querySelector('#address').innerHTML = 'Kigali Rwanda';
+	// document.querySelector('#street').innerHTML = 'KN 2 Avenue';
+	document.querySelector('#email').innerHTML = 'bukreative@gmail.com';
+	document.querySelector('#phone').innerHTML = '+250788801095';
 
 	// Link to social media
 	document.querySelector('#facebook').href = 'http://www.facebook.com';
 	document.querySelector('#twitter').href = 'http://www.twitter.com';
 	document.querySelector('#instagram').href = 'https://www.instagram.com/bukreative/';
-	document.querySelector('#whatsapp').href = 'https://wa.me/+250788384757';
+	document.querySelector('#whatsapp').href = 'https://wa.me/+250788801095';
 	document.querySelector('#youtube').href = 'http://www.youtube.com';
 	document.querySelector('#linkedin').href = 'http://www.linkedin.com';
 }
@@ -122,13 +118,13 @@ const serviceImages = [
 	{ image: './images/dress_1.jpg', title: 'LOOK GOOD, FEEL GOOD', text: 'If you like looking great and you struggle with your outfit choices, I would like to help you bring out the best of your wardrobe while letting you express yourself in the best way' },
 	{
 		image: './images/earrings_1.jpg',
-		title: 'Sweaters',
-		text: 'Best quality Sweaters',
+		title: 'ICONIC EARRINGS',
+		text: 'We have a wide range of creatively designed earrings made from Rwanda',
 	},
-	{ image: './images/earrings_2.jpg', title: 'T Shirts', text: 'Boys T-Shirts' },
-	{ image: './images/earrings_3.jpg', title: 'Dresses', text: 'Girl dresses' },
-	{ image: './images/skirt_1.jpg', title: 'Combo', text: 'Top and short' },
-	{ image: './images/dress_2.jpg', title: 'Combo', text: 'Top and short' },
+	{ image: './images/earrings_2.jpg', title: 'GREAT EARRINGS', text: 'A wide range of creatively designed earrings made from Rwanda' },
+	{ image: './images/earrings_3.jpg', title: 'MORE EARRINGS', text: 'Good quality earrings designed and made from Rwanda' },
+	{ image: './images/skirt_1.jpg', title: 'FASHIONABLE DRESS', text: 'Hire me and I’ll spend a significant time with you helping you discover what goes with your body type and the vast range of styles you would certainly rock' },
+	{ image: './images/dress_2.jpg', title: 'MORE DRESSES', text: 'A perfect fit' },
 ];
 
 function changeImages() {
@@ -145,7 +141,7 @@ function changeImages() {
 		i = 0;
 		j = 0;
 	}
-	setTimeout('changeImages()', 3000);
+	setTimeout('changeImages()', 10000);
 }
 window.onload = changeImages;
 
